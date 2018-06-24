f764bdb1dfc0acdca4b3935190b2ad93a192ea1a
Dhaivat Vyas <devdkapps@gmail.com>
2018-06-24 23:22:04 +0530

    made changes by adding alamofire and calling api.

--- JSONParsingSwift4/ViewController.swift
+++ JSONParsingSwift4/ViewController.swift
@@ -7,19 +7,54 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    var arrArticles = [Articles]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        callApi()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func callApi() {
+        let strURL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fe79aa80ed7b49f5bdf68592100a2b96"
+        
+        let url = URL(string: strURL)!
+        Alamofire.request(url).responseData { (responseData) in
+            do {
+                let Data = try JSONDecoder().decode(newsResponse.self, from: responseData.data!)
+                self.arrArticles = Data.articles!
+                print("self.arrArticles", self.arrArticles)
+            } catch let err {
+                print(err)
+            }
+        }
     }
-
-
+    
 }
 
+struct newsResponse: Decodable {
+    let status: String?
+    let totalResults: Int?
+    let articles: [Articles]?
+    
+}
+
+struct Articles: Decodable {
+    let source : Source?
+    let author: String?
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    
+}
+
+struct Source: Decodable {
+    let id: String?
+    let name: String?
+}
